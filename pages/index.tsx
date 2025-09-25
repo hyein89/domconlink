be9bd7cdc60bf95eca83f57aecfa9cd5dd7741f3
hyein89 <bihontai@gmail.com>
2025-09-26 02:13:33 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ export default function Home() {
       <Head>
         <title>Coming Soon</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-         <link rel="stylesheet" href="/home.css" />
+        {/* CSS dari public/home.css */}
+        <link rel="stylesheet" href="/home.css" />
       </Head>
 
       <div className="container text-center">
@@ -22,6 +23,7 @@ export default function Home() {
                 <path d="M480 288c0-80.25-49.28-148.92-119.19-177.62L320 192V80a16 16 0 0 0-16-16h-96a16 16 0 0 0-16 16v112l-40.81-81.62C81.28 139.08 32 207.75 32 288v64h448zm16 96H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h480a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path>
               </svg>
             </div>
+
             <h1 className="animated fadeIn">We're working on it!</h1>
             <div className="description-text animated fadeIn delay-1s">
               <p>This site is currently under construction.</p>
