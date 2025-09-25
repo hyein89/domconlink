13747e90f498b1ddcd47a932ae00224f1ca126c9
hyein89 <bihontai@gmail.com>
2025-09-26 00:19:41 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{textAlign:"center", padding:"40px"}}>
+      <h1>Landing Kosong</h1>
+      <p>Isi halaman utama di sini.</p>
+    </main>
+  );
+}
